a2a698d193277c3ed68120ed9869e94f51b6fcee
viewweiwu <648764190@qq.com>
2020-09-07 22:40:22 +0800

    fix: sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:viewweiwu/vue-tabs-chrome.git master:gh-pages
+git push -f https://github.com/viewweiwu/vue-tabs-chrome.git master:gh-pages
 
 cd -
